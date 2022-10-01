c5a1ecc11048d5d3f6fdbf451e72c1311c8050d2
stdio <SGDiomov18@codingburgas.bg>
2022-10-01 13:54:02 +0300

    Homework

---
+++ src/Autowired.java
@@ -0,0 +1,2 @@
+public @interface Autowired {
+}
---
+++ src/Dog.java
@@ -0,0 +1,24 @@
+@Component
+@Autowired
+public class Dog {
+
+
+    private String name;
+    private int age;
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge(int age) {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
